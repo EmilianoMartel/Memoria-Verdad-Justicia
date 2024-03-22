94f358e4f7f88376196924e8374434228828989a
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2024-03-22 17:29:05 -0300

    Update 0.1

---
+++ Assets/Scripts/Sound/VolumeSettings.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeSettings : MonoBehaviour
+{
+    [SerializeField] private AudioMixer _audioMixer;
+}
